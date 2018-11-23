e8f156a62c77f6032b243e23dcb24e62c45757eb
Renaro <renaropsantos@gmail.com>
2018-11-23 16:23:02 +0100

    Show how to create observables and subscribe to it

--- app/src/test/java/com/renarosantos/reactivecoursesampleapp/ExampleUnitTest.kt
+++ app/src/test/java/com/renarosantos/reactivecoursesampleapp/ExampleUnitTest.kt
@@ -1,5 +1,8 @@
 package com.renarosantos.reactivecoursesampleapp
 
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.Single
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -10,8 +13,34 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun creatingObservable() {
+        Observable.just(1,2,3,4,5,6)
+                .doOnSubscribe{
+                    println("Subscribed")
+                }
+                .doOnNext{
+                    println("Value = $it")
+                }
+                .doOnComplete { println("Completed") }
+                .doOnError{ println("Error")}
+                .subscribe()
+
+        println("Test Finished")
+    }
+
+    @Test
+    fun createSingle(){
+        Single.just("Single")
+                .doOnSuccess {  println(it)}
+                .subscribe()
+    }
+
+    @Test
+    fun createCompletable(){
+        Completable.complete()
+                .doOnComplete{ print("Completed")}
+                .subscribe()
     }
 }
