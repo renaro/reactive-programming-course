b7d0d27c29314d26815206c0fc2d90995e0ee50b
Renaro Santos <renaropsantos@gmail.com>
2018-11-26 20:33:33 +0100

    Adding more observable examples

--- app/src/test/java/com/renarosantos/reactivecoursesampleapp/ExampleUnitTest.kt
+++ app/src/test/java/com/renarosantos/reactivecoursesampleapp/ExampleUnitTest.kt
@@ -5,8 +5,6 @@ import io.reactivex.Observable
 import io.reactivex.Single
 import org.junit.Test
 
-import org.junit.Assert.*
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -16,31 +14,51 @@ class ExampleUnitTest {
 
     @Test
     fun creatingObservable() {
-        Observable.just(1,2,3,4,5,6)
-                .doOnSubscribe{
-                    println("Subscribed")
-                }
-                .doOnNext{
-                    println("Value = $it")
-                }
+        Observable.just(1, 2, 3, 4, 5, 6)
+                .doOnSubscribe { println("Subscribed") }
+                .doOnNext { println("Value = $it") }
                 .doOnComplete { println("Completed") }
-                .doOnError{ println("Error")}
+                .doOnError { println("Error") }
                 .subscribe()
 
         println("Test Finished")
     }
 
     @Test
-    fun createSingle(){
+    fun createSingle() {
         Single.just("Single")
-                .doOnSuccess {  println(it)}
+                .doOnSuccess { println(it) }
                 .subscribe()
     }
 
     @Test
-    fun createCompletable(){
+    fun createSingleError() {
+        Single.error<Int>(IllegalArgumentException())
+                .doOnSuccess { println(it) }
+                .doOnError { println(it) }
+                .subscribe()
+    }
+
+    @Test
+    fun createCompletable() {
         Completable.complete()
-                .doOnComplete{ print("Completed")}
+                .doOnComplete { print("Completed") }
+                .subscribe()
+    }
+
+    @Test
+    fun createCompletableWithError() {
+        Completable.error(IllegalArgumentException())
+                .doOnComplete { print("Completed") }
+                .subscribe()
+    }
+
+    @Test
+    fun takeFirstTwo() {
+        Observable.just(21, 22, 23, 24, 25, 26)
+                .takeLast(2)
+                .filter { it -> it > 24 }
+                .doOnNext { println("$it") }
                 .subscribe()
     }
 }
